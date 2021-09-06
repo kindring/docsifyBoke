1b22fa1d76a9b1c6c2ac665e28f6f57726bf55fb
kindring <2460131004@qq.com>
2021-09-06 18:02:42 +0800

    更新配置文件加載部份

--- controller/boke/boke.js
+++ controller/boke/boke.js
@@ -1,8 +1,8 @@
 /*
  * @Author: kindring
  * @Date: 2021-08-25 14:18:54
- * @LastEditTime: 2021-08-25 17:55:54
- * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2021-09-06 18:02:28
+ * @LastEditors: kindring
  * @Description: 博客处理
  * @FilePath: \docsifyBoke\controller\boke.js
  */
@@ -11,29 +11,43 @@ const fs = require('fs');
 const path = require('path')
 
 const loadTools = require('../../loadTool')
-const bokeConfig = require('../../configs/boekServer');
+
+// 导入加载配置文件
+const Config = require('./config')
 
 const { error } = require('console');
 
-const {handel,getConfig} = loadTools;
+const { handel, getConfig } = loadTools;
+
+const bokeConfigController = new Config();
+let bokeConfig = null;
+let isLoaded = false;
+bokeConfigController.onloaded = function() {
+    isLoaded = true;
+    bokeConfig = bokeConfigController.getConfig();
+    startServer(bokeConfig);
+}
 
 // 封装任务
-async function startServer() {
-    let [gitErr,gitCloneStd] = await loadTools.handel(gitClone())
-    if(gitErr){ throw gitErr; }
-    //  启动服务,非异步存储
-    startDocsify(bokeConfig.rootPath,bokeConfig.port);
-    
+async function startServer(bokeConfig) {
+    // 克隆任务
+    let [gitErr, gitCloneStd] = await loadTools.handel(gitClone(bokeConfig))
+    let [gitPullErr, gitPullStr] = await loadTools.handel(gitPull(bokeConfig))
+    if (gitErr) { throw gitErr; }
 
+    //  启动服务,非异步存储
+    startDocsify(bokeConfig.rootPath, bokeConfig.port);
 }
 
 // 克隆仓库
-function gitClone() {
-    return new Promise((resolve,reject)=>{
+function gitClone(bokeConfig) {
+    return new Promise((resolve, reject) => {
+        console.log(bokeConfig)
+        console.log('----')
         let docPath = path.join(bokeConfig.rootPath, bokeConfig.repositoryName)
         console.log(docPath)
         console.log(fs.existsSync(docPath))
-        if (fs.existsSync(docPath)) { return resolve(); } 
+        if (fs.existsSync(docPath)) { return resolve(); }
         let cd = exec(`git clone ${bokeConfig.cloneUrl}`, { cwd: bokeConfig.rootPath }, (err, stdout, stderr) => {
             if (err) {
                 console.log(err)
@@ -42,14 +56,38 @@ function gitClone() {
             console.log(stdout)
             console.log(`stdout`)
             console.log(`stderr`)
-            console.log(stderr) 
+            console.log(stderr)
             resolve(stderr);
         })
     })
 }
+
+// 拉取最新倉庫
+function gitPull(bokeConfig) {
+    return new Promise((resolve, reject) => {
+        console.log(bokeConfig)
+        console.log('----')
+        let docPath = path.join(bokeConfig.rootPath, bokeConfig.repositoryName)
+        console.log(docPath)
+        console.log(fs.existsSync(docPath))
+        if (fs.existsSync(docPath)) { return resolve(); }
+        let gp = exec(`git pull`, { cwd: path.join(docPath) }, (err, stdout, stderr) => {
+            if (err) {
+                console.log(err)
+                return reject(err);
+            }
+            console.log(stdout)
+            console.log(`stdout`)
+            console.log(`stderr`)
+            console.log(stderr)
+            resolve(stderr);
+        })
+    })
+}
+
 // 启动服务
-function startDocsify(rootPath,port){
-    return new Promise((resolve,reject)=>{
+function startDocsify(rootPath, port) {
+    return new Promise((resolve, reject) => {
         exec(`yarn docsify start ${rootPath} --port ${port}`, (err, stdout, stderr) => {
             if (err) {
                 console.log(err)
@@ -58,36 +96,36 @@ function startDocsify(rootPath,port){
             console.log(stdout)
             console.log(`stdout`)
             console.log(`stderr`)
-            console.log(stderr) 
+            console.log(stderr)
             resolve(stderr)
         });
     })
 }
-
 // 生成侧边栏
-async function updateSideBar(){
-    let [configErr,config] = await handel(getConfig('./config'));
-    if(configErr){return deadlyErrorHandel(configErr,'配置文件加载')}
+async function updateSideBar() {
+    let [configErr, config] = await handel(getConfig('./config'));
+    if (configErr) { return deadlyErrorHandel(configErr, '配置文件加载') }
     // 获取目录下的所有文件
-    let dirs = loadFile(config.docPath,'',config.maxChildLevel);
+    let dirs = loadFile(config.docPath, '', config.maxChildLevel);
     // console.log(dirs);
     // 排除目标文件
-    dirs = excludePath(dirs,config.docPath,config.excludePath);
+    dirs = excludePath(dirs, config.docPath, config.excludePath);
     // 过滤一级文件夹中没有子项的部分
-    dirs = dirs.filter(val=>val.isDirectory?val.children.length>0:true);
+    dirs = dirs.filter(val => val.isDirectory ? val.children.length > 0 : true);
     console.table(dirs);
-    let siderbarString = await createSidebarString(dirs,config.basePath,config.docPath,config.isCreateTmpReadme);
+    let siderbarString = await createSidebarString(dirs, config.basePath, config.docPath, config.isCreateTmpReadme);
     let sidebarTarget = path.join(config.sidebarTarget)
-    let sideBar_md_exist =  await fs.existsSync(sidebarTarget);
-    let [writeError,isWriteOk] = await handel(writePromise(sidebarTarget,siderbarString))
-    if(writeError){ return deadlyErrorHandel(writeError,'写入文件失败') }
+    let sideBar_md_exist = await fs.existsSync(sidebarTarget);
+    let [writeError, isWriteOk] = await handel(writePromise(sidebarTarget, siderbarString))
+    if (writeError) { return deadlyErrorHandel(writeError, '写入文件失败') }
     console.log('侧边栏写入成功 1');
 }
 
 
 
 
-startServer();
+// startServer();
+
 
 module.exports = {
     startServer,
--- controller/boke/config.js
+++ controller/boke/config.js
@@ -4,10 +4,11 @@
  * @Autor: kindring
  * @Date: 2021-08-31 17:52:23
  * @LastEditors: kindring
- * @LastEditTime: 2021-09-03 17:46:00
+ * @LastEditTime: 2021-09-06 18:01:07
  */
 
 const fs = require('fs')
+const { resolve } = require('path')
 const path = require('path')
 const loadTools = require('../../loadTool')
 const { handel, getConfig } = loadTools
@@ -19,12 +20,14 @@ class Config {
     constructor() {
         this.config = this.baseConfig
         this.configsUrl = '../../configs/boke.json'
-        this.localConfig()
+        this.loadConfig()
     }
-    static baseConfig = {
-        docsifyPath: '../../boke', //博客文件的目录
+    baseConfig = {
+        port: 8988,
+
+        docsifyPath: '/boke', //博客文件的目录
         repositoryName: 'doc', // github仓库目录
-        cloneUrl: '', // git 仓库地址
+        cloneUrl: 'url', // git 仓库地址
         header: 'master',
         excludeExtName: ['.git', 'README.md'],
         homeTarget: 'README.md', //首页文件位置,只基于doc目录下的文件来进行处理
@@ -35,8 +38,19 @@ class Config {
         plugin: [], //使用的插件
     }
 
-    // 插件表.
-    static plugin = {
+
+    positionMap = {
+            /** body后面 */
+            bodyEnd: 'body后面',
+            /** body前面 */
+            bodyStart: 'body前面',
+            /** docsify前面 */
+            docsifyStart: 'docsify前面',
+            /** docsify后面 */
+            docsifyEnd: 'docsify后面'
+        }
+        // 插件表.
+    plugin = {
         'gittalk': {
             name: '评论插件',
             descript: '采用github账号实现评论功能',
@@ -64,52 +78,50 @@ class Config {
             }
         }
     }
-    static positionMap = {
-        /** body后面 */
-        bodyEnd: 'body后面',
-        /** body前面 */
-        bodyStart: 'body前面',
-        /** docsify前面 */
-        docsifyStart: 'docsify前面',
-        /** docsify后面 */
-        docsifyEnd: 'docsify后面'
-    }
+
+
 
     // 加载配置文件
-    async loadConfig() {
-        let [configErr, config] = await handel(getConfig(configsUrl))
-        if (configErr) {
-            // 配置文件加载错误,直接生成一个新的配置文件
-            this.saveConfig()
-            config = {}
-        }
-        // 填充配置文件
-        config = {...baseConfig, config }
+    loadConfig() {
+        return new Promise(async resolve => {
+            let [configErr, config] = await handel(getConfig(require.resolve(this.configsUrl)))
+            if (configErr) {
+                // 配置文件加载错误,直接生成一个新的配置文件
+                this.saveConfig()
+                config = {}
+                console.log(configErr);
+            }
+            // 填充配置文件
+            config = {...this.baseConfig, ...config }
+                // config.excludeExtName
+            config.excludeExtName.push({
+                path: config.tmpReadmeName,
+                include: true, //目录中包含此字符串即可
+            })
 
-        config.excludeExtName.push({
-            path: config.tmpReadmeName,
-            include: true, //目录中包含此字符串即可
+            config.excludeExtName.push({
+                path: config.tmpReadmeName,
+                include: true, //目录中包含此字符串即可
+            })
+
+            // 扩展一些常用的配置文件
+            config.rootPath = path.join(__dirname, '../../' + config.docsifyPath)
+            this.config = config
+            this.onloaded()
+            resolve();
         })
-
-        config.excludeExtName.push({
-            path: config.tmpReadmeName,
-            include: true, //目录中包含此字符串即可
-        })
-
-        // 扩展一些常用的配置文件
-        config.rootPath = path.join(__dirname, this.docsifyPath)
-
-        this.config = config
     }
 
+    getConfig() {
+        return this.config;
+    }
 
     saveConfig() {
         return new Promise((resolve, reject) => {
-
             // 保存当前的配置,持久化
             let jsonConfig = JSON.stringify(this.config)
                 // let isExits = fs.existsSync(this.configsUrl)
-            fs.writeFile(this.configsUrl, jsonConfig, (err) => {
+            fs.writeFile(path.join(__dirname, this.configsUrl), jsonConfig, (err) => {
                 if (err) { return reject(err) }
                 resolve()
             })
@@ -121,4 +133,11 @@ class Config {
         this.config[item] = val;
 
     }
+
+
+    onloaded() {}
+
 }
+
+
+module.exports = Config;
